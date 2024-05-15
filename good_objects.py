636a50d865a6a400a948514e84aebae192cdfadd
ShengFeng Li <cyeba8369211@gmail.com>
2024-05-15 19:19:12 +0800

    test v1

---
+++ good_objects.py
@@ -0,0 +1,55 @@
+import requests
+
+def ROC_year(year):
+    if year < 1912:
+        raise ValueError("ROC year start from 1912 !")
+    else:
+        return int(year - 1911)
+    
+crops= {
+    'Maize': '002',
+    'Wheat': '003',
+    'Soybean': '101',
+    'Tomato': '425',
+    'Sugarcane': '306',
+    'Taro': '440',
+    'Cabbage': '419'
+}
+
+items = {
+    'winter': '00',
+    'first_season': '01',
+    'second_seanson': '02',
+    'whole_year': '03'
+}
+
+def afa_scratch(year: int, item: str, crop: str, url: str = 'https://agr.afa.gov.tw/afa/pgcropcity.jsp'):
+    if not 86 <= year <= 110:
+        raise ValueError("Invalid year data")
+    
+    # Request data
+    payload = {
+        'accountingyear': str(year),
+        'item': item,
+        'corn001': '',
+        'input803': '',
+        'crop': crop,
+        'city': '00',
+        'btnSend': '送　出'
+    }
+
+    try:
+        response = requests.post(url, data = payload)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4XX, 5XX)
+        print('Success')  # Outputs the response text directly
+        return response
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Oops: Something Else", err)
+
+
---
+++ main.py
@@ -0,0 +1,40 @@
+from good_objects import ROC_year, afa_scratch, crops, items
+from bs4 import BeautifulSoup
+import pandas as pd
+import itertools
+
+
+# Assuming you have lists of parameters that correspond by index
+ROC_years = [ROC_year(y) for y in range(1997, 2022)]
+
+# Use zip to iterate over matched elements
+for year, item, crop in itertools.product(ROC_years, items.keys(), crops.keys()):
+
+    response = afa_scratch(year, items[item], crops[crop])
+
+    # Parse the HTML
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Find the table
+    div = soup.find('div', class_='DivRestTbl')
+
+    rows = div.find('table', class_= "TDFont")
+
+    # Extract rows
+    rows = rows.find_all('tr')
+    # Process each row to extract column data
+    data = []
+    for row in rows:
+        cols = [ele.text.strip() for ele in row.find_all('td')]
+        if cols:  # Avoid empty lists from empty rows
+            data.append(cols)
+
+    # Create a DataFrame with the extracted data
+    # First row of data contains the headers
+    if not ['查無資料！！'] in data:
+        df = pd.DataFrame(data[1:], columns=data[0])
+    else: 
+        print(f"{(year, item, crop)} not found.")
+        next
+    
+    df.to_csv(f"./response/{crop}_{year}_{item}.csv", index = False, encoding = 'big5')
